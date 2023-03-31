959ac6307b00c3dcc8c52b6b8dfda259e615a439
Higas01 <higor.porangaba123@hotmail.com>
2023-03-31 11:55:18 -0300

    finalizando back-end

--- backend/serve.js
+++ backend/serve.js
@@ -5,7 +5,7 @@ const router = require("./routes");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors({ methods: "*" }));
+app.use(cors({ methods: "*", credentials: true }));
 app.use(router);
 
 app.listen(3000, async (req, res) => {
