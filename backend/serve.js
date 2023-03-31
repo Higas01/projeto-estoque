83ee063bebbdfab980a4f2187b149ab865b751eb
Higas01 <higor.porangaba123@hotmail.com>
2023-03-31 11:11:54 -0300

    enviando arquivos para deloy

--- backend/serve.js
+++ backend/serve.js
@@ -23,19 +23,17 @@ const {
 const validationLogin = require("./middlewares/ValidationLogin");
 const deleteToken = require("./middlewares/DeleteToken");
 
-const router = Router();
-
-router.post("/register", validationRegister, registerUser);
-router.post("/login", validationLogin, loginUser);
-router.delete("/logout", deleteToken, (req, res) => {
+app.post("/register", validationRegister, registerUser);
+app.post("/login", validationLogin, loginUser);
+app.delete("/logout", deleteToken, (req, res) => {
   res.status(200).json({ message: "logout com sucesso!" });
 });
 
-router.post("/products/add", verifyToken, addItemController);
-router.get("/products", verifyToken, getItemController);
-router.get("/products/:id", verifyToken, getItemIDController);
-router.put("/products/update/:id", verifyToken, updateItemController);
-router.delete("/products/delete/:id", verifyToken, deleteItemController);
+app.post("/products/add", verifyToken, addItemController);
+app.get("/products", verifyToken, getItemController);
+app.get("/products/:id", verifyToken, getItemIDController);
+app.put("/products/update/:id", verifyToken, updateItemController);
+app.delete("/products/delete/:id", verifyToken, deleteItemController);
 
 app.listen(3000, async (req, res) => {
   await db();
