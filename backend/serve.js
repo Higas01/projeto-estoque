858b152fadc651de7cb457c7c676667d9126526d
Higas01 <higor.porangaba123@hotmail.com>
2023-03-31 11:34:20 -0300

    finalizando backend

--- backend/serve.js
+++ backend/serve.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-const routes = require("./routes");
+const router = require("./routes");
 
 app.use(express.json());
-app.use(routes);
+app.use(router);
 
 app.listen(3000, async (req, res) => {
   await db();
