19accb5251facaf61908c6117ae5c58d3ea68270
Higas01 <higor.porangaba123@hotmail.com>
2023-04-29 11:04:31 -0300

    enviando algo

--- frontend/src/Pages/RegisterProducts/RegisterProducts.tsx
+++ frontend/src/Pages/RegisterProducts/RegisterProducts.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./RegisterProducts.module.css";
+import { BsPencil, BsTrash } from "react-icons/bs";
 
 type Props = {};
 
@@ -9,12 +10,44 @@ const RegisterProducts = (props: Props) => {
       <div className={styles.container}>
         <h1 className={styles.h1}>Controle seu estoque aqui</h1>
         <p className={styles.p}>Registre um produto abaixo</p>
-        <input
-          type="button"
-          value="Registrar produto"
-          className={styles.btn}
-          onClick={() => console.log("hello word")}
-        />
+      </div>
+      <div>
+        <button>Registre um produto</button>
+        <table className={styles.table}>
+          <thead>
+            <th>Produto</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Tamanho (opcional)</th>
+            <th>Validade (opcional)</th>
+          </thead>
+          <tr>
+            <td>Calça</td>
+            <td>29.99</td>
+            <td>M</td>
+            <td> - </td>
+            <td> - </td>
+            <td>
+              <BsPencil />
+            </td>
+            <td>
+              <BsTrash />
+            </td>
+          </tr>
+          <tr>
+            <td>Calça</td>
+            <td>29.99</td>
+            <td>M</td>
+            <td> - </td>
+            <td> - </td>
+            <td>
+              <BsPencil />
+            </td>
+            <td>
+              <BsTrash />
+            </td>
+          </tr>
+        </table>
       </div>
     </section>
   );
