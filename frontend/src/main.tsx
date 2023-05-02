50720f390504b8d94ccad8c307de444e473d66cb
Higas01 <higor.porangaba123@hotmail.com>
2023-05-02 11:07:57 -0300

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 );
